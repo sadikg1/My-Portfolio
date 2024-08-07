61df9f03780b8e3ccff8e762ed6ed7288aeb0d5c
Sadeek Ghimire <sadeekg133@gmail.com>
2024-08-07 19:15:37 +0545

    Final Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,47 +7,85 @@ const About = () => {
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
-          <img src={AboutImage} alt=""  className="w-72 h-80 rounded object-cover mb-8 md:mb-0"></img>
+          <img
+            src={AboutImage}
+            alt=""
+            className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
+          ></img>
           <div className="flex-1">
             <p className="text-lg mb-8">
-              I am a dedicated
-              full-stack developer with a passion for creating modern, responsive
-              web applications. With a solid foundation in both frontend and
-              backend technologies, I aim to deliver seamless and efficient user
-              experiences.
+              Hi, I’m Sadik Ghimire, a passionate Final-year Student currently
+              pursuing a BSc.CSIT. My journey in tech is fueled by a deep interest in data
+              and analytics, driving me to master React, MongoDB, Express, and
+              Node.js. I am dedicated to continuous learning and keeping up with
+              the latest advancements in machine learning. Alongside coding, I
+              actively engage in open-source projects and seek out opportunities
+              for growth. I look forward to connecting and collaborating on
+              innovative projects that make a meaningful impact in the tech
+              world.
             </p>
             <div className="space-y-4">
               <div className="flex items-center">
-                <label htmlFor="htmlandcss" className="w-2/12">HTML &CSS</label>
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  HTML &CSS
+                </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
-                <div
+                  <div
                     className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
                     transform transition-transform duration-300 hover:scale-105 w-10/12"
                   ></div>
                 </div>
               </div>
               <div className="flex items-center">
-                <label htmlFor="htmlandcss" className="w-2/12">React JS</label>
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  React JS
+                </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
-                <div
+                  <div
                     className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
                     transform transition-transform duration-300 hover:scale-105 w-8/12"
                   ></div>
                 </div>
               </div>
               <div className="flex items-center">
-                <label htmlFor="htmlandcss" className="w-2/12">Node.js</label>
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Node.js
+                </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
-                <div
+                  <div
                     className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
                     transform transition-transform duration-300 hover:scale-105 w-7/12"
                   ></div>
                 </div>
               </div>
               <div className="flex items-center">
-                <label htmlFor="htmlandcss" className="w-2/12">Python Programming</label>
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  PHP
+                </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
-                <div
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-6/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  SQL
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-8/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Python Programming
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
                     className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
                     transform transition-transform duration-300 hover:scale-105 w-7/12"
                   ></div>
@@ -55,28 +93,33 @@ const About = () => {
               </div>
             </div>
             <div className="mt-12 flex justify-between text-center">
-            <div>
-                    <h3 className="text-2xl font-bold text-transparent bg-clip-text 
-                bg-gradient-to-r from-green-400 to-blue-500">
-                        3+
-                    </h3>
-                    <p>Years Experience</p>
-                </div>
-                <div>
-                    <h3 className="text-2xl font-bold text-transparent bg-clip-text 
-                bg-gradient-to-r from-green-400 to-blue-500">
-                        10+
-                    </h3>
-                    <p>Projects Completed</p>
-                </div>
-                <div>
-                    <h3 className="text-2xl font-bold text-transparent bg-clip-text 
-                bg-gradient-to-r from-green-400 to-blue-500">
-                        5+
-                    </h3>
-                    <p>Happy Clients</p>
-                </div>
-
+              <div>
+                <h3
+                  className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500"
+                >
+                  3+
+                </h3>
+                <p>Years Experience</p>
+              </div>
+              <div>
+                <h3
+                  className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500"
+                >
+                  10+
+                </h3>
+                <p>Projects Completed</p>
+              </div>
+              <div>
+                <h3
+                  className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500"
+                >
+                  5+
+                </h3>
+                <p>Happy Clients</p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,10 +33,10 @@ const Home = () => {
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
           Sadik Ghimire
         </span>
-        ,Full-Stack Developer
+        ,React Learner
       </h1>
       <p className="mt-4 text-lg text-gray-300">
-        I specialize in building modern and responsive web applications.
+        I'm specializing in building modern and responsive web applications.
       </p>
       <div className="mt-8 space-x-4">
         <a
