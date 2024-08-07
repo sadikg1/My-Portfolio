fd59f893f89beebe4770417c488c10d6f8971ad2
Sadik Ghimire <109445737+sadikg1@users.noreply.github.com>
2024-08-07 19:19:31 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='bg-black text-white px-8 md:px-16 lg:px24' id='nav'>
-      <div className='container py-2 flex justify-center md:justify-between items-center'>
-      <div>
-          <img  src='/Sname.png' className='h-12 w-auto hidden md:inline' alt='Logo' />
+    <nav className="bg-black text-white px-8 md:px-16 lg:px24" id="nav">
+      <div className="container py-2 flex justify-center md:justify-between items-center">
+        <div>
+          <img
+            src="/Sname.png"
+            className="h-12 w-auto hidden md:inline"
+            alt="Logo"
+          />
         </div>
-      <div className='space-x-6'>
-        <a href='#home'className='hover:text-gray-400'>Home</a>
-        <a href='#about'className='hover:text-gray-400'>About Me</a>
-        <a href='#services'className='hover:text-gray-400'>Services</a>
-        <a href='#projects'className='hover:text-gray-400'>Projects</a>
-        <a href='#contact'className='hover:text-gray-400'>Contact</a>
-      </div>
-      <a href='#contact' className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</a>
+        <div className="space-x-6">
+          <a href="#home" className="hover:text-gray-400">
+            Home
+          </a>
+          <a href="#about" className="hover:text-gray-400">
+            About Me
+          </a>
+          <a href="#services" className="hover:text-gray-400">
+            Services
+          </a>
+          <a href="#projects" className="hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#contact" className="hover:text-gray-400">
+            Contact
+          </a>
+        </div>
+        <a
+          href="https://www.linkedin.com/in/sadeek-ghimire-094192245/"
+          className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+        >
+          Connect Me
+        </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
